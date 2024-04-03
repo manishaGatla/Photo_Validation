68c62fd6cb729387a32c05eeecf9f51d83fb81f4
Kiran Kumar Kongari <kongarikiran77@gmail.com>
2024-04-03 00:12:22 -0500

    recent changes

--- src/app/home-component/home-component.component.ts
+++ src/app/home-component/home-component.component.ts
@@ -1,64 +1,135 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ImageCaptureServiceService } from '../image-capture-service.service';
-
+import { ElementRef } from '@angular/core';
+import { ViewChild } from '@angular/core';
 @Component({
   selector: 'app-home-component',
   templateUrl: './home-component.component.html',
   styleUrls: ['./home-component.component.scss']
 })
-export class HomeComponentComponent implements OnInit{
+export class HomeComponentComponent implements OnInit {
+  @ViewChild('videoElement') videoElement!: ElementRef<HTMLVideoElement>;
+  @ViewChild('canvas') canvas!: ElementRef<HTMLCanvasElement>;
   uploadImageLabel: string = 'Choose a file to upload';
-  user: any ={
-    name : null,
-    email : null,
-  };  
-  public urlOfImage:string = '';
-  public selectedFile:any ;
 
-  constructor(public service: ImageCaptureServiceService){}
-  ngOnInit():void{
+  user: any = {
+    name: null,
+    email: null,
+    password: null
+  };
+  public urlOfImage: string = '';
+  public selectedFile: any;
+  capturedImageUrl: any;
 
+  constructor(public service: ImageCaptureServiceService) { }
+  ngOnInit(): void {
   }
 
 
 
-  
 
-  onFileSelected(event : any): void {
-      this.urlOfImage="";
-      const selectedfile: File = event.target.files[0];
-      this.selectedFile = {
-        file :selectedfile
-      };
-      var reader = new FileReader();
-      reader.readAsDataURL(selectedfile);
-      reader.onload = (res :any)=>{
-        if(selectedfile.type !== "text/plain"){
-          this.urlOfImage = res.target.result;
-        }
+
+  onFileSelected(event: any): void {
+    this.urlOfImage = "";
+    const selectedfile: File = event.target.files[0];
+    this.selectedFile = {
+      file: selectedfile
+    };
+    var reader = new FileReader();
+    reader.readAsDataURL(selectedfile);
+    reader.onload = (res: any) => {
+      if (selectedfile.type !== "text/plain") {
+        this.urlOfImage = res.target.result;
       }
+    }
   }
 
-  loginUser(){
+  loginUser() {
+    this.startCamera();
+    const payload = {
+      "email": this.user.email,
+      "username": this.user.name,
+      "password": this.user.password
+    }
+    this.captureImage();
+    console.log(this.capturedImageUrl);
+    // this.service.callLambdaFunctionForLogin(payload).subscribe((res: any)=>{
+    //   if(res){
 
+    //   }
+    // })
   }
 
-  registerUser(){
-
+  startCamera(): void {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then(stream => {
+          this.videoElement.nativeElement.srcObject = stream;
+          this.videoElement.nativeElement.play()
+            .catch((error: any) => {
+              if (error.name === "NotAllowedError") {
+                alert('Please allow camera access and refresh the page.');
+              }
+              console.error('Error trying to play the video stream', error);
+            });
+        })
+        .catch(error => {
+          console.error('Error getting user media:', error);
+          if (error.name === "NotAllowedError") {
+            alert('Please allow camera access and refresh the page.');
+          }
+        });
+    } else {
+      console.error('Media devices are not supported by this browser.');
+    }
   }
 
-  backClicked(){
-    this.service.isLoginClicked= false;
+
+  registerUser() {
+    const payload = {
+      "email": this.user.email,
+      "username": this.user.name,
+      "password": this.user.password,
+      "profile_picture_url": `https://verify-id-webapp.s3.amazonaws.com/profile_pictures/${this.user.name}/profile.jpg`
+    }
+    this.service.callLambdaFunctionForRegister(payload).subscribe((res: any) => {
+      if (res) {
+        this.service.uploadFile(this.selectedFile.file as File, `https://verify-id-webapp.s3.amazonaws.com/profile_pictures/${this.user.name}/profile.jpg`).subscribe({
+          next: () => {
+            alert('Upload successful');
+            this.reset();
+          },
+          error: () => alert('Upload failed'),
+        });
+      }
+    })
+  }
+
+  backClicked() {
+    this.service.isLoginClicked = false;
     this.service.isRegisterClicked = false;
     this.reset();
   }
 
-  reset(){
-    this.user ={
-      name : null,
-      email : null,
-    };  
+  reset() {
+    this.user = {
+      name: null,
+      email: null,
+      password: null
+    };
     this.urlOfImage = '';
   }
 
+
+  captureImage(): void {
+    const context = this.canvas.nativeElement.getContext('2d');
+    if (context) {
+      const width = this.videoElement.nativeElement.videoWidth;
+      const height = this.videoElement.nativeElement.videoHeight;
+      this.canvas.nativeElement.width = width;
+      this.canvas.nativeElement.height = height;
+      context.drawImage(this.videoElement.nativeElement, 0, 0, width, height);
+      this.capturedImageUrl = this.canvas.nativeElement.toDataURL('image/png');
+    }
+  }
 }
--- src/app/image-capture-service.service.ts
+++ src/app/image-capture-service.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -7,8 +8,31 @@ export class ImageCaptureServiceService {
   isLoginClicked: boolean = false;
   isRegisterClicked: boolean= false;
   isLoginSuccessful: boolean = false;
-  
-  constructor() { }
 
- 
+  private apiGatewayEndpointForRegister = 'https://xlxhu4v6r9.execute-api.us-east-1.amazonaws.com/stage1/';
+  private apiGatewayEndpointForLogin = 'https://xlxhu4v6r9.execute-api.us-east-1.amazonaws.com/stage1/';
+
+  constructor(private http: HttpClient) { }
+
+  public callLambdaFunctionForRegister(payload: any) {
+    return this.http.post(this.apiGatewayEndpointForRegister, payload);
+  }
+
+  public callLambdaFunctionForLogin(payload: any) {
+    return this.http.post(this.apiGatewayEndpointForLogin, payload);
+  }
+
+  getPresignedUrl(fileName: string) {
+    return this.http.get<{ url: string, key: string }>('https:localhost:3200/api/getPresignedUrl', { params: { fileName } });
+  }
+
+  uploadFile(file: File, url: string) {
+    const formData: FormData = new FormData();
+    formData.append('file', file);
+    const headers = new HttpHeaders({
+      'Content-Type': file.type
+    });
+    return this.http.put(url, file, { headers }); 
+  }
 }
+
